735d06f061bd052565172885cf6d71f92927a478
Kayann Soares <kayan.soares1@gmail.com>
2023-04-11 22:50:11 -0300

    Update compare_models

--- HW1/compare_models.m
+++ HW1/compare_models.m
@@ -1,9 +1,10 @@
 clc
+clear 
 
-% Modelo não linear do pêndulo invertido
+% Modelo Não Linear do Pêndulo Invertido
 func = @inverted_pend;
 
-% Modelo linear do pêndulo invertido
+% Modelo Linear do Pêndulo Invertido
 x0 = [0, 0, 0, 0];
 
 A = [0 1 0 0;
@@ -18,10 +19,12 @@ C = [1 0 0 0;
      0 0 1 0];
 D = [0;
      0];
+
+
 sys = ss(A, B, C, D);
 
 % Definifição do tempo
-tspan = 0:0.01:10;
+tspan = 0:0.1:5;
 
 for type = ["step", "sin", "ramp"]
     [t, x] = ode45 (@(t,y) inverted_pend(t,y,type) , tspan , x0);
@@ -31,9 +34,9 @@ for type = ["step", "sin", "ramp"]
     % Gráficos das saídas
     figure;
     subplot(2,1,1);
-    plot(t, x(:,1), 'LineWidth', 2);
+    plot(t, rem(x(:,1),2*pi), 'LineWidth', 2);
     hold on;
-    plot(t, y_linear(:,1), 'LineWidth', 2);
+    plot(t, rem(y_linear(:,1),2*pi), 'LineWidth', 2);
     legend('Não linear', 'Linear');
     legend("Position", [0.15681,0.61039,0.17894,0.075058])
     title('Posição angular');
@@ -42,7 +45,7 @@ for type = ["step", "sin", "ramp"]
     grid on;
     
     subplot(2,1,2);
-    plot(t, x(:,2), 'LineWidth', 2);
+    plot(t, x(:,3), 'LineWidth', 2);
     hold on;
     plot(t, y_linear(:,2), 'LineWidth', 2);
     legend('Não linear', 'Linear');
@@ -51,4 +54,11 @@ for type = ["step", "sin", "ramp"]
     xlabel('Tempo (s)');
     ylabel('$x$ (m)', 'Interpreter', 'latex');
     grid on; 
+
+    rmse_theta = rmse(rem(x(:,1),2*pi),rem(y_linear(:,1),2*pi));
+    rmse_x = rmse(x(:, 3),y_linear(:, 2));
+
+    disp(type)
+    disp(["RMSE theta:", rmse_theta])
+    disp(["RMSE x:", rmse_x])
 end
