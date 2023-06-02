feaf6a6121667b30c48fd16cdd5a7ed0c362e712
Kayann Soares <kayan.soares1@gmail.com>
2023-06-02 17:28:43 -0300

    Update PID_controller_design.m

--- HW2/PID_controller_design.m
+++ HW2/PID_controller_design.m
@@ -12,6 +12,7 @@ G2 = H/G1;
 
 PD1 = kp1 + kd1*s;
 PD2 = kp2 + kd2*s;
+
 % Teste individaul dos cpntroladores
 
 cl1 = (PD1*G1)/(1+PD1*G1);
@@ -23,11 +24,13 @@ cl2 = (PD2*H)/(1+PD2*H);
 simplify(cl2);
 [nl2, dl2] = numden(cl2);
 routh(dl2,s)
+
 % Definição da Função de transferência do Loop interno
 
 Tii = G1/(1+G1*PD1);
 [nii, dii] = numden(Tii);
 routh(dii, s)
+
 % Para que a função seja estável, temos que kd1 < 0 e kp1 < -47.088.
 % Escolhemos kd1 = -3 e kp1 = -50
 
@@ -36,17 +39,20 @@ PD1 = simplify(PD1);
 
 Tii = subs(Tii, [kd1, kp1], [-3, -50]);
 Tii = simplify(Tii)
+
 % Para obter a equação do controlador fazemos:
 
 response_1 = 1/(tc*s+1);
 
 C1 = (response_1/(Tii*(1-response_1)));
 C1 = simplify(C1)
+
 % Vemos que C1 é um PID dado por:
 % $$ kd_{1} = \frac{-2.688}{t_{c}} \; ; \; kp_{1} = \frac{-3}{t_{c}} \; ; \;  
 % ki_{1} = \frac{-2.912}{t_{c}}$$
 
 G2
+
 % Obtendo a equação de malha fechada para o loop externo
 
 closed_1 = simplify(C1*Tii/(1+C1*Tii));
@@ -55,6 +61,7 @@ To = closed_1*G2;
 Too = simplify(To/(1 + To*PD2));
 [noo, doo] = numden(Too);
 routh(doo, s)
+
 % Pelas condições retornadas, temos:
 % $$0 < kp2 < \frac{100}{56}\\kp2 \cdot t_{c} < kd2 < \frac{100 \cdot t_{c}}{56}$$
 % Escolhemos kp2 = 0.9 e kd2 = 1.4*tc
@@ -64,11 +71,13 @@ PD2 = simplify(PD2);
 
 Too = subs(Too, [kd2, kp2], [1.4*tc, 0.9]);
 Too = simplify(Too)
+
 % Para obter a  função do controlador, fazemos
 
 response_2 = (w^2)*(1 - 0.239*s)/(s^2 + 2*z*w*s + w^2);
 C2 = (response_2/(Too*(1-response_2)));
 C2 = simplify(C2)
+
 % Ecolhendo o valor de tc
 % Fazemos tc = 0.5
 
@@ -79,6 +88,7 @@ C2 = subs(C2, tc, 0.5);
 C2 = simplify(C2)
 C1 = subs(C1, tc, 0.5);
 C1 = simplify(vpa(C1))
+
 % Refatorando C2
 % $$-\frac{w^2 \cdot (108s^3 + 496s^2 + 6867s + 8829)}{10s \cdot (s + 2wz) \cdot 
 % (56s^2 - 981)} = -\frac{w^2 \cdot (s+1.3822)\cdot(s+1.6052 - 7.5213 i)\cdot(s+1.6052 
@@ -96,17 +106,20 @@ C1 = simplify(vpa(C1))
 
 z = -log(2/100)/sqrt(pi^2 + (log(2/100))^2)
 w = 2.5/(z*3)
+
 % Substituindo em C2
 
 C2 = w^2*(s^2 + 3.2104*s + 59.1461)/(9810*s);
 digits(4);
 C2 = simplify(vpa(C2))
+
 %%
 PD1
 PD2
 C1
 C2
-%%
+
+%% Função Routh–Hurwitz
 function M = routh(eq, var)
     coeff = coeffs(eq, var, "All");
     l = length(coeff);
